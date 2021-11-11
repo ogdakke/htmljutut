631610f067eb8c2397ac7b60eb054f7a42419fa0
ogdakke <daniel.wargh1@gmail.com>
2021-11-12 00:52:01 +0200

    nonii

--- email-algo.php
+++ email-algo.php
@@ -1,16 +1,16 @@
 <?
-$mail_to_send_to = "name@anydomain.tld";
 $from_email = "daniel.wargh@danielew.com";
 $sendflag = $_REQUEST['sendflag'];    
 $name=$_REQUEST['name'];
+$lastname=$_REQUEST['lastname']
 if ( $sendflag == "send" )
         {
                 $subject= "Message subject";
                 $email = $_REQUEST['email'] ;
-                $message= "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
+                $message= "\r\n" . "Name: $name $lastname" . "\r\n"; //get recipient name in contact form
                 $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
                 $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
-                $a = mail( $mail_to_send_to, $subject, $message, $headers );
+                $a = mail( $email, $subject, $message, $headers );
                 if ($a)
                 {
                      print("Message was sent, you can send another one");
