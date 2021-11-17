674ecc5b04a7011c9f6ec19b38747d8408204639
ogdakke <daniel.wargh1@gmail.com>
2021-11-17 17:35:36 +0200

    404 page lisäys, jotain

--- email-algo.php
+++ email-algo.php
@@ -47,116 +47,116 @@
             </li>      
         </ul>
     </nav>
-    <main>
-        <section class="form">
-            <aside>
-                <h2 id="h2-form" >contact form</h2>
-            </aside>
-            <div class="form-container">
-                <h2 id="h2-form">Send me an email about anything.</h2>
-                <div class="form-wrapper">
-                    <form method="POST" action="email-algo.php" class="contact-form" style="opacity: 1;">
-                        <div class="form-name-lastname">
-                            <label for="name" class="text-muted">First Name*</label>
-                                <input type="text" class="form-input" name="name" placeholder="First Name" required="">
-                            <label for="lastname" class="text-muted">Last Name</label>
-                                <input type="text" class="form-input" name="lastname" placeholder="Last Name">
-                        </div>
-                        <label for="email" class="text-muted">Email</label>
-                            <input type="email" class="form-input" name="email" placeholder="Your@email.here" required="">
-                        <label for="" class="text-muted"></label>
-                            <textarea name="message" id="" cols="30" rows="6" maxlength="600" placeholder="Write your message here"></textarea>
-                        <input class="button" name="sendflag" type="submit" value="send">
-                        <div class="php-hope" >
-     <?php
-               $mail_to_send_to = "daniel.wargh1@gmail.com";
-               $from_email = "daniel.wargh@danielew.com";
-               $sendflag = $_REQUEST['sendflag'];    
-               $name=$_REQUEST['name'];
-               $lastname=$_REQUEST['lastname'];
-               if ( $sendflag == "send" )
-                    {
-                    $subject= "Message subject";
-                    $email = $_REQUEST['email'] ;
-                    $message= "\r\n" . "Name: $name $lastname" . "\r\n"; //get recipient name in contact form
-                    $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
-                    $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
-                    $a = mail( $mail_to_send_to, $subject, $message, $headers );
-                    if ($a)
-                    {
-                         
+<main>
+    <section class="form">
+        <aside>
+            <h2 id="h2-form" >contact form</h2>
+        </aside>
+        <div class="form-container">
+            <h2 id="h2-form">Send me an email about anything.</h2>
+            <div class="form-wrapper">
+                <form method="POST" action="email-algo.php" class="contact-form" style="opacity: 1;">
+                    <div class="form-name-lastname">
+                        <label for="name" class="text-muted">First Name*</label>
+                            <input type="text" class="form-input" name="name" placeholder="First Name" required="">
+                        <label for="lastname" class="text-muted">Last Name</label>
+                            <input type="text" class="form-input" name="lastname" placeholder="Last Name">
+                    </div>
+                    <label for="email" class="text-muted">Email</label>
+                        <input type="email" class="form-input" name="email" placeholder="Your@email.here" required="">
+                    <label for="" class="text-muted"></label>
+                        <textarea name="message" id="" cols="30" rows="6" maxlength="600" placeholder="Write your message here"></textarea>
+                    <input class="button" name="sendflag" type="submit" value="send">
+                    <div class="php-hope" >
+    <?php
+            $mail_to_send_to = "daniel.wargh1@gmail.com";
+            $from_email = "daniel.wargh@danielew.com";
+            $sendflag = $_REQUEST['sendflag'];    
+            $name=$_REQUEST['name'];
+            $lastname=$_REQUEST['lastname'];
+            if ( $sendflag == "send" )
+                {
+                $subject= "Message subject";
+                $email = $_REQUEST['email'] ;
+                $message= "\r\n" . "Name: $name $lastname" . "\r\n"; //get recipient name in contact form
+                $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
+                $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
+                $a = mail( $mail_to_send_to, $subject, $message, $headers );
+                if ($a)
+                {
+                        
 
-                         $toimii = 'Your message has been delivered.';
-                         echo "<p style='color:white;'>" . $toimii . "</p>";
-                    } else {
-                         $eitoimi = "Your message wasn't sent. Please check your email address.";
-                         echo "<p style='color:white;'>" . $eitoimi . "</p>";
-                    }
-          }
-     ?>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
-        <section class="footer-page">
-            <div class="footer-container">
-                <footer id="footer1">
-                    <div class="footer-logo-box" >
-                        <img src="nc_assets/kuvat/logo/Asset_2.svg" alt="logo" id="footer_logo" title="logo">
-                        <div class="footer-text">
-                            <p class="text-muted" title="address">01610 Vantaa, Finland</p>
-                            <p class="text-muted" title="copyright">&COPY Daniel Wargh 2021</p>
-                        </div>
-                    </div>
-                </footer>
-                <footer id="footer2">
-                    <ul class="socials">
-                        <li> 
-                            <a class="link-social" href="https://twitter.com/">Twitter</a>
-                        </li>
-                        <li>
-                            <a class="link-social" href="https://www.linkedin.com/">Linkedin</a>
-                        </li>
-                        <li>
-                            <a class="link-social" href="https://www.youtube.com/">Youtube</a>
-                        </li>
-                    </ul>
-                </footer>
-                <footer id="footer3">
-                    <div class="contact-box-upper">
-                        <div class="button-wrapper">
-                            <input href="" class="button" id="newsletter-button" type="submit" name="sendflag" value="send" />
-                        </div>
-                    </div>
-                    <div class="contact-box-lower">
-                        <div class="newsletter-wrapper">
-                            <form action="" class="newsletter" style="opacity: 1;">
-                                <div class="email-form">
-                                    <label for="EMAIL" class="text-muted">Enter your email to maybe someday get something there.</label>
-                                    <input type="email" name="EMAIL" class="form-input" placeholder="your@email.here">
-                                    
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </footer>  
-            </div>
-    </section>
-        <!-- script for hiding navbar while scrolling down -->
-        <script >
-            var prevScrollpos = window.pageYOffset;
-            window.onscroll = function() {
-                var currentScrollPos = window.pageYOffset;
-                if (prevScrollpos > currentScrollPos) {
-                    document.getElementById("navbar-id").style.bottom = "0";
+                        $toimii = 'Your message has been delivered.';
+                        echo "<p style='color:white;'>" . $toimii . "</p>";
                 } else {
-                    document.getElementById("navbar-id").style.bottom = "-5rem";
+                        $eitoimi = "Your message wasn't sent. Please check your email address.";
+                        echo "<p style='color:white;'>" . $eitoimi . "</p>";
                 }
-                prevScrollpos = currentScrollPos;
-            } 
-        </script>
-    </main>
+        }
+    ?>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <section class="footer-page">
+        <div class="footer-container">
+            <footer id="footer1">
+                <div class="footer-logo-box" >
+                    <img src="nc_assets/kuvat/logo/Asset_2.svg" alt="logo" id="footer_logo" title="logo">
+                    <div class="footer-text">
+                        <p class="text-muted" title="address">01610 Vantaa, Finland</p>
+                        <p class="text-muted" title="copyright">&COPY Daniel Wargh 2021</p>
+                    </div>
+                </div>
+            </footer>
+            <footer id="footer2">
+                <ul class="socials">
+                    <li> 
+                        <a class="link-social" href="https://twitter.com/">Twitter</a>
+                    </li>
+                    <li>
+                        <a class="link-social" href="https://www.linkedin.com/">Linkedin</a>
+                    </li>
+                    <li>
+                        <a class="link-social" href="https://www.youtube.com/">Youtube</a>
+                    </li>
+                </ul>
+            </footer>
+            <footer id="footer3">
+                <div class="contact-box-upper">
+                    <div class="button-wrapper">
+                        <input href="" class="button" id="newsletter-button" type="submit" name="sendflag" value="send" />
+                    </div>
+                </div>
+                <div class="contact-box-lower">
+                    <div class="newsletter-wrapper">
+                        <form action="" class="newsletter" style="opacity: 1;">
+                            <div class="email-form">
+                                <label for="EMAIL" class="text-muted">Enter your email to maybe someday get something there.</label>
+                                <input type="email" name="EMAIL" class="form-input" placeholder="your@email.here">
+                                
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </footer>  
+        </div>
+</section>
+    <!-- script for hiding navbar while scrolling down -->
+    <script >
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos) {
+                document.getElementById("navbar-id").style.bottom = "0";
+            } else {
+                document.getElementById("navbar-id").style.bottom = "-5rem";
+            }
+            prevScrollpos = currentScrollPos;
+        } 
+    </script>
+</main>
     
 </body>
 </html>
